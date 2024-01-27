0f23d738fb6f73a6f86146f0515dad08c6f8468c
Adora <adoraonyile@gmail.com>
2024-01-27 13:36:24 +0100

    fixin' checker errors

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-sumres = add(a, b)
-difres = sub(a, b)
-prdres = mul(a, b)
-divres = div(a, b)
-print("{} + {} = {}".format(a, b, sumres))
-print("{} - {} = {}".format(a, b, difres))
-print("{} * {} = {}".format(a, b, prdres))
-print("{} / {} = {}".format(a, b, divres))
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    sumres = add(a, b)
+    difres = sub(a, b)
+    prdres = mul(a, b)
+    divres = div(a, b)
+    print("{} + {} = {}".format(a, b, sumres))
+    print("{} - {} = {}".format(a, b, difres))
+    print("{} * {} = {}".format(a, b, prdres))
+    print("{} / {} = {}".format(a, b, divres))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
-if len(argv) == 1:
-    print("0 arguments.")
-else:
-    print("{} argument{}:".format(len(argv) - 1, "s" if len(argv) > 2 else ""))
-    for i, arg in enumerate(argv[1:], 1):
-        print("{}: {}".format(i, arg))
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    suffix = 's' if argc > 1 else ''
+    if len(argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(argc, suffix), end=' ')
+        for i, arg in enumerate(argv[1:], 1):
+            print("{}: {}".format(i, arg))
