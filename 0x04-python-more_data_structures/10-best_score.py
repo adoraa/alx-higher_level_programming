9bfba3e1b7bd4b66f23af28d55ba2fa6d83771a3
Adora <adoraonyile@gmail.com>
2024-01-27 17:02:14 +0100

    returns a key with the biggest integer value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        max_key = max(a_dictionary, key=a_dictionary.get)
+        return max_key
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    pass
