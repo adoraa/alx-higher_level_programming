32b4a6fed63a7c89418eadc5c4bff9e7864c212e
Adora <adoraonyile@gmail.com>
2024-01-27 13:20:01 +0100

    calc

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+sumres = add(a, b)
+difres = sub(a, b)
+prdres = mul(a, b)
+divres = div(a, b)
+print("{} + {} = {}".format(a, b, sumres))
+print("{} - {} = {}".format(a, b, difres))
+print("{} * {} = {}".format(a, b, prdres))
+print("{} / {} = {}".format(a, b, divres))
