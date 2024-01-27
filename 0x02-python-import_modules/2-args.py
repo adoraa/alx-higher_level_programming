a1271e927c9dd344174fc6ea3753f8a90f1bf2b2
Adora <adoraonyile@gmail.com>
2024-01-27 13:56:45 +0100

    fixing errs

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,11 @@ from sys import argv
 if __name__ == "__main__":
     argc = len(argv) - 1
     suffix = 's' if argc > 1 else ''
-    if len(argv) == 1:
+    if argc == 0:
         print("0 arguments.")
     else:
-        print("{} argument{}:".format(argc, suffix), end=' ')
+        print("{} argument{}:".format(argc, suffix))
         for i, arg in enumerate(argv[1:], 1):
             print("{}: {}".format(i, arg))
+        if argc == 1:
+            print("1: {}".format(argv[1]))
