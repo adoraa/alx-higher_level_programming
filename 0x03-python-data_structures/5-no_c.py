c5d12111714ce280ba65898be1131787903e857d
Adora <adoraonyile@gmail.com>
2024-01-27 15:24:54 +0100

    fixin errors

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,11 +3,11 @@ def no_c(my_string):
     new_string = ""
     for c in my_string:
         if c not in ['c', 'C']:
-            new_string += char
+            new_string += c
     return new_string
 
 
 if __name__ == "__main__":
-    print(no_c("Best School"))
-    print(no_c("Chicago"))
-    print(no_c("C is fun!"))
+    word = input()
+    new_word = no_c(word)
+    print(new_word)
