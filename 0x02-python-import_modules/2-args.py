8d0f39cc1e707f4f481c0fe40dffe926da0afda7
Adora <adoraonyile@gmail.com>
2024-01-27 13:23:07 +0100

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import argv
+if len(argv) == 1:
+    print("0 arguments.")
+else:
+    print("{} argument{}:".format(len(argv) - 1, "s" if len(argv) > 2 else ""))
+    for i, arg in enumerate(argv[1:], 1):
+        print("{}: {}".format(i, arg))
