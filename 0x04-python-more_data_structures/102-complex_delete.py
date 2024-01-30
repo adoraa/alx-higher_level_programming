71863282ee6a141bf008712741b4ec6d844c006d
Adora <adoraonyile@gmail.com>
2024-01-30 02:35:52 +0100

    delete by value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -5,9 +5,9 @@ def complex_delete(a_dictionary, value):
     return {key: val for key, val in a_dictionary.items() if val != value}
 
 
-def print_sorted_dictionary(a_dict):
-    if not a_dict:
+def print_sorted_dictionary(new_dict):
+    if not new_dict:
         return
-    for key in sorted(a_dict.keys()):
-        print("{}: {}".format(key, a_dict[key]))
+    for key in sorted(new_dict.keys()):
+        print("{}: {}".format(key, new_dict[key]))
     print("--")
