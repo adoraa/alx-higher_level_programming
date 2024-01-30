8c2c44cbd6353c90f299e0d56c72f9394996dfc5
Adora <adoraonyile@gmail.com>
2024-01-30 02:47:17 +0100

    pycodestyle

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -5,9 +5,11 @@ def complex_delete(a_dictionary, value):
         del a_dictionary[key]
     return a_dictionary
 
+
 def print_sorted_dictionary(a_dictionary):
     for key in sorted(a_dictionary.keys()):
         print("{}: {}".format(key, a_dictionary[key]))
+
 
 if __name__ == "__main__":
     pass
