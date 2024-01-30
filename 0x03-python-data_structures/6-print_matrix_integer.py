6c5bc3cc55f7a9ef2bb31464ef7bb71916bab53e
Adora <adoraonyile@gmail.com>
2024-01-30 02:53:52 +0100

    debugging

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for n in row:
-            print("{:d}".format(n), end=" ")
+            print("{:d}".format(n), end='')
         print()
 
 
