dbda67c141948995406cb027d51807f0ffc93df9
Adora <adoraonyile@gmail.com>
2024-01-30 02:19:33 +0100

    delete by value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    if not a_dictionary:
+        return a_dictionary
     return {key: val for key, val in a_dictionary.items() if val != value}
+
+
+def print_sorted_dictionary(new_dict):
+    if not new_dict:
+        return
+    for key in sorted(new_dict.keys()):
+        print("{}: {}".format(key, new_dict[key]))
+    print("--")
