6853a24a0a1a55b726a6d6b7f20f0c749517b8b8
Adora <adoraonyile@gmail.com>
2024-02-07 02:14:32 +0100

    ByteCode -> Python #5 - the Python class MagicClass that worls like a Python bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import math
+
+
+class Magic:
+    def __init__(self, radius=0):
+        """
+        Initializes a new instance of the MagicClass class.
+
+        Args:
+            radius: The radius of the circle (default is 0).
+        """
+        self.__radius = 0
+
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+
+        self.__radius = radius
+
+    def area(self):
+        """
+        Calculates the area of the circle.
+
+        Returns:
+            float: The area of the circle.
+        """
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """
+        Calculates the circumference of the circle.
+
+        Returns:
+            float: The circumference of the circle.
+        """
+        return 2 * math.pi * self.__radius
+
+
+if __name__ == "__main__":
+    m_circle = Magic(radius=5)
+
+    print("Radius:", m_circle._Magic__radius)
+    print("Area:", m_circle.area())
+    print("Circumference:", m_circle.circumference())
