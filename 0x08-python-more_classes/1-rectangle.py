68764609c35be785d30658eed55931e826f40545
Adora <adoraonyile@gmail.com>
2024-02-13 04:26:06 +0100

    defines a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""Defines a rectangle"""
+
+
+class Rectangle:
+    """
+    Rectangle class with private attributes width and height.
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a new instance of the Rectangle class.
+
+        Args:
+            width (int): width of the rectangle.
+            height (int): height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        Getter method for width.
+
+        Returns:
+            int: width of the rectangle.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter method for width.
+
+        Args:
+            value (int): value to set as the width.
+
+        Raises:
+            TypeError: If value ! an integer.
+            ValueError: If value < than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter method for height.
+
+        Returns:
+            int: height of the rectangle.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter method for height.
+
+        Args:
+            value (int): value to set as the height.
+
+        Raises:
+            TypeError: If value !an integer.
+            ValueError: If value < than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
