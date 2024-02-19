eb023961cadf72c491e974dfd69ba7706fb98113
Adora <adoraonyile@gmail.com>
2024-02-19 21:37:05 +0100

    a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py)

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class BaseGeometry:
+    """Base class representing base geometry"""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates an integer value
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Class representing a rectangle"""
+
+    def __init__(self, width, height):
+        """Instantiates a rectangle"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculates the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns the formatted rectangle description"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """Class representing a square"""
+
+    def __init__(self, size):
+        """Instantiates a square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Returns the formatted square description."""
+        hght = self._Rectangle__height
+        wdth = self._Rectangle__width
+        return "[Square] {}/{}".format(wdth, hght)
