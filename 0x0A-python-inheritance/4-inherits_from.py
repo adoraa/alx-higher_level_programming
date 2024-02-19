acd2bb744081167a6a4399fc3e907fdb8f366222
Adora <adoraonyile@gmail.com>
2024-02-19 20:32:46 +0100

    Checks if an object is an instance of a class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Checks if an object is an instance of a class"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that inherited
+    from the specified class; otherwise False.
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
