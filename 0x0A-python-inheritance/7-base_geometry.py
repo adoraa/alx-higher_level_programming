0088e3c80b9c8772c6ee30bd5b2aaa2c8e703353
Adora <adoraonyile@gmail.com>
2024-02-19 21:15:42 +0100

    a class BaseGeometry (based on 6-base_geometry.py)

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Creates a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Empty class representing base geometry."""
+
+    def area(self):
+        """
+        Raises an exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates an integer value
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
