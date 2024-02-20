5265b818f21ee2455d8348e4288e9a04eec1f083
Adora <adoraonyile@gmail.com>
2024-02-21 00:27:22 +0100

    returns the JSON representation of an object (string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module that defines a function to return the
+JSON representation of an object (string).
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: The object to be serialized.
+
+    Returns:
+        str: The JSON representation of the object.
+    """
+    return json.dumps(my_obj)
