3debb8cd481a618364dff498e87ff3f3e72bfc7b
Adora <adoraonyile@gmail.com>
2024-02-27 02:46:09 +0100

    updated the class Base by adding the class method def create(cls, **dictionary): that returns an instance with all attributes already set

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,10 @@ class Base:
         if not json_string:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes already set."""
+        dummy_instance = cls(1, 1)
+        dummy_instance.update(**dictionary)
+        return dummy_instance
