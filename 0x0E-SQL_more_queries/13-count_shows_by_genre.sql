9e05f644b1e03d094ff204c34ee1a2c1a451d92d
Adora <adoraonyile@gmail.com>
2024-04-01 13:51:48 +0100

    13 checker error fix

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Select genre and count of shows linked to each genre
-SELECT tv_show_genres.genre AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+SELECT tv_show_genres.genre AS genre, COUNT(*) AS number_of_shows
 FROM tv_show_genres
 JOIN tv_show_genres ON tv_show_genres.id = tv_show_genres.id
 GROUP BY tv_show_genres.name
