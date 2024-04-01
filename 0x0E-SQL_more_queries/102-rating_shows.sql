774aa1f710a320ca3138e3a5180506799bdaca43
Adora <adoraonyile@gmail.com>
2024-04-01 17:02:02 +0100

    102 checker error fix

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- List shows by their rating sum
 SELECT title, SUM(rate) AS rating
 FROM tv_shows
-INNER JOIN tv_shows_ratings ON tv_shows.id = tv_shows_ratings.show_id
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY title
 ORDER BY rating DESC;
