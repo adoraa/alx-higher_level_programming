f1aeb4cabfb9017fb71b375d24707c70c7e76379
Adora <adoraonyile@gmail.com>
2024-04-01 13:34:17 +0100

    script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Create the database hbtn_0d_usa with table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.tates (
-    PRIMARY KEY(id),
+    PRIMARY KEY(`id`),
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL
 );
