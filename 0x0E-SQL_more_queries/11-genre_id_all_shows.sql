e944fad70a8dd60c83d9d75e709eb5965dea4b10
Adora <adoraonyile@gmail.com>
2024-04-01 13:47:36 +0100

    11 checker error fix

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- Select tv_shows.title and tv_show_genres.genre_id from tv_shows left joined with tv_show_genres
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
