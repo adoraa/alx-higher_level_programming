eb24e2142c599e272e4d0c1738c944a5aabfa48b
Adora <adoraonyile@gmail.com>
2024-04-25 15:11:47 +0100

    prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects from the database hbtn_0e_6_usa
+Prints the first State object from the database hbtn_0e_6_usa
 """
 
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import session
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
@@ -16,7 +16,10 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(first_state.id, first_state.name))
     session.close()
