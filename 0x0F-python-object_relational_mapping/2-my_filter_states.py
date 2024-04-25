fda49e83aa3da3c365d2ccf1b93130ba52c62eff
Adora <adoraonyile@gmail.com>
2024-04-25 14:49:30 +0100

    takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table where name matches the argument.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+    state_name = sys.argv[4]
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY states.id ASC".format(state_name,))
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
