b4b44fcd0aee9b2937098cc1d7d4fc68f7700c86
Adora <adoraonyile@gmail.com>
2024-04-25 14:58:56 +0100

    script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+    state_name = sys.argv[4]
+    cursor.execute("SELECT * FROM states WHERE name=%s\
+                ORDER BY states.id ASC", (state_name,))
+
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
