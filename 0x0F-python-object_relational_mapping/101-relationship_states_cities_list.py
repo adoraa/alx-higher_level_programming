c2cda050f260085da3d5b4d3c1f12566463eb483
Adora <adoraonyile@gmail.com>
2024-04-28 17:43:19 +0100

    lists all City objects from the database

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Lists all State objects and corresponding City objects"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.schema import Table
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
+
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Lists all City objects and their corresponding State objects"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.schema import Table
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    cities = session.query(City).order_by(City.id).all()
+    for city in cities:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+
+    session.close()
