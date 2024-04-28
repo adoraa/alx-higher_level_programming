5f872e92f8b4c36cb3ff9498ed05d01edf805633
Adora <adoraonyile@gmail.com>
2024-04-28 16:46:50 +0100

    cheker fail fix

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    Base.metadata.create_all(engine)
 
+    session = Session(engine)
     for state in session.query(State)\
                         .filter(State.name.like('%a%'))\
                         .order_by(State.id):
