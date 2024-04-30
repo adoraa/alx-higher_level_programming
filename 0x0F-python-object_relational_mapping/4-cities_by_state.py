023d37d3b65f0adbba47974c0553e4947cfe4f98
Adora <adoraonyile@gmail.com>
2024-04-30 02:26:52 +0100

    checker fail

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -20,6 +20,6 @@ if __name__ == "__main__":
                     JOIN states ON cities.state_id = states.id \
                         ORDER BY cities.id ASC")
 
-    row = cursor.fetchall()
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
