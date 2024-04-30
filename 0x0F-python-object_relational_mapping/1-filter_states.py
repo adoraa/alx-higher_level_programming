47cee170643ac7a06dd5bee7cce2ba1cca8ba1aa
Adora <adoraonyile@gmail.com>
2024-04-30 02:18:02 +0100

    checker fail

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,10 +15,10 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name
-                  LIKE 'N%' ORDER BY states.id ASC")
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states WHERE name
+                  LIKE BINARY 'N%' ORDER BY states.id ASC""")
 
-    rows = cur.fetchall()
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
