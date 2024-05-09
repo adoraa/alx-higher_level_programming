1980f53f29ae78b43ab46e14a5a123a99d0390e8
Adora <adoraonyile@gmail.com>
2024-05-09 16:02:50 +0100

    checker fail

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a request to 0.0.0.0:5000/catch_me with a custom User-Agent header
-curl -s -X PUT -d "user_id=98" -H "Origin: You got me!" -L 0.0.0.0:5000/catch_me
+curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
