54d22ba8066a31d599832d17bb51cc9a990ed97b
Adora <adoraonyile@gmail.com>
2024-05-10 13:11:27 +0100

    takes in a URL, sends a request to the URL and displays the body of the response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Sends a request to a URL and displays the body of the response."""
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
